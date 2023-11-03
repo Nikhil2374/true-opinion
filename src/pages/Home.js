1d900639bd06ead6c1d1dbdde74e46f88e651a91
Nikhil2374 <133630341+Nikhil2374@users.noreply.github.com>
2023-11-03 14:29:04 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,9 @@ function Home(){
     return <div>
         <div className="section">
             <div className="content">
-                <h1>Create, Share Surveys Easily</h1>
+                <h1>Create, Share Quiz Easily</h1>
                 <div className='home-box'>
-                <p>TrueOpinion allows you to create surveys in just a few clicks. Create a free account now and get started with a new adventure of opinions . Share the link among your friends , family and colleagues and capture their response . It's Simple, Secure, and safe because Your opinion matters! </p>
+                <p>Welcome to TrueOpinion Quiz! TrueOpinion Quiz is your ultimate destination for fun and engaging quizzes on a wide range of topics. Whether you're a trivia enthusiast, a pop culture aficionado, or simply looking to test your knowledge, our website has something for everyone.Explore a diverse collection of quizzes that cover everything from history and science to movies, music, and current events. Challenge yourself and your friends to see who has the sharpest mind or the most in-depth knowledge on various subjects. </p>
                 <Link to="/create" className="btn">get started</Link>
                 </div>
             </div>
